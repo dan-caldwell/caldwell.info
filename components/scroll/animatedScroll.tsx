fd8a69ae50ca93a05395ff6ac83c1e0ff6b99cdd
dan-caldwell <dan@caldwell.info>
2021-11-23 22:25:20 -0500

    add animated scrolling prototype

--- components/scroll/animatedScroll.tsx
+++ components/scroll/animatedScroll.tsx
@@ -1,9 +1,49 @@
+import { useEffect, useState } from "react";
 
-const AnimatedScroll = () => {
+type AnimatedScrollProps = {
+    children: React.ReactNode[]
+}
+
+const timelineMin = 0;
+const timelineMax = 6000;
+
+const AnimatedScroll: React.FC<AnimatedScrollProps> = ({ children }) => {
+    const [scrollTop, setScrollTop] = useState(0);
+
+    const handleScroll = e => {
+        setScrollTop(window.scrollY);
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const childMultiple = timelineMax / children.length;
+    const currentChild = Math.floor(scrollTop / childMultiple);
+    const progress = (scrollTop / childMultiple) - currentChild + 0.5;
+
     return (
-        <div className="sticky top-0 bg-yellow-300">
-            Hello world
-        </div>
+        <>
+            <div
+                style={{
+                    height: timelineMax + 'px'
+                }}
+            >
+                <div 
+                    className="sticky"
+                    style={{
+                        top: `50vh`,
+                        transform: `translate(0, -50%) perspective(100rem) rotateY(${(180 * progress) - 180}deg)`
+                    }}
+                >
+                    {children.map((child, index) => (
+                        <div key={index} className={currentChild === index ? "block" : "hidden"}>{child}</div>
+                    ))}
+                </div>
+            </div>
+
+        </>
     )
 }
 
